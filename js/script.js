edbc81283b4cc52347dd61c4143bc4a78fb338d9
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2022-12-21 10:54:47 +0100

    fixed

--- js/script.js
+++ js/script.js
@@ -71,11 +71,3 @@ createApp({
         }
     }
 }).mount('#app');
-
-var input = document.getElementById("myInput");
-input.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    document.getElementById("myBtn").click();
-  }
-});
