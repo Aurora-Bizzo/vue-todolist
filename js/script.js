d4c1a0c4cca82a078bf544cf555d909c900f954e
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2022-12-21 03:57:47 +0100

    completed with error on enterkey

--- js/script.js
+++ js/script.js
@@ -3,18 +3,79 @@ const { createApp } = Vue;
 createApp({
     data(){
         return {
+            newTask: '',
             toDoList:
             [
-               'Fare le pulizie',
-               'Comprare un regalo per mamma',
-               'Giocare con i gatti per almeno un ora',
-               'Andare dal parrucchiere',
-               'Montare il nuovo mobile del soggiorno',
-               'Prendere appuntamento dal dentista',
-               'Rammendare la biancheria',
-               'Ripassare la lezione precedente',
-               'Fare i compiti'
+               {
+                text: 'Fare le pulizie',
+                done: false,
+               },
+               {
+                text: 'Comprare un regalo per mamma',
+                done: false,
+               },
+               {
+                text: 'Giocare con i gatti per almeno un ora',
+                done: false,
+               },
+               {
+                text: 'Andare dal parrucchiere',
+                done: false,
+               },
+               {
+                text: 'Montare il nuovo mobile del soggiorno',
+                done: false,
+               },
+               {
+                text: 'Prendere appuntamento dal dentista',
+                done: false,
+               },
+               {
+                text: 'Rammendare la biancheria',
+                done: false,
+               },
+               {
+                text: 'Ripassare la lezione precedente',
+                done: false,
+               },
+               {
+                text: 'Fare i compiti',
+                done: false,
+               }
             ]
         }
+    },
+    methods: {
+        addTask()
+        {
+            let object = {
+                text: this.newTask,
+                done: false,
+            }
+            this.toDoList.push(object);
+            this.newTask = '';
+        },
+        deleteTask(index)
+        {
+            this.toDoList.splice(index, 1);
+        },
+        checkTask(index)
+        {
+            let task = this.toDoList[index];
+            if(task.done == true){
+                task.done = false;
+            }
+            else{
+                task.done = true;
+            }
+        }
     }
-}).mount('#app')
+}).mount('#app');
+
+var input = document.getElementById("myInput");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("myBtn").click();
+  }
+});
